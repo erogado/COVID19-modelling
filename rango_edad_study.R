49afa810490976a8b6637267553451227570e3b2
erogado <38254601+erogado@users.noreply.github.com>
2020-03-29 20:20:51 +0200

    Delete rango_edad_study.R

--- rango_edad_study.R
+++
@@ -1,7 +0,0 @@
-url = "https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/nacional_covid19_rango_edad.csv"
-rango_edad = read.csv(url)
-
-colnames(rango_edad) = c("Fecha", "sexo", "rango_edad", "casos_confirmados", 
-                         "hospitalizados", "ingresados_uci", "fallecidos")
-
-rango_edad
